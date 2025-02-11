0933c538d7719ca30111ce0bfce5c44c317f1ad0
CarlosHernandez14 <carloskvpchc@gmail.com>
2025-02-10 19:35:08 -0600

    First commit, spring security basics

---
+++ src/test/java/com/charly/eazybank/EazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charly.eazybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
